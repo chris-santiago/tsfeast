97a7402be9c7ac4f14cf4d288b9ab02f42a9bc9e
chris-santiago <cjsantiago@gatech.edu>
2021-08-03 22:51:58 -0400

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,11 +90,10 @@ napoleon_use_admonition_for_examples = True
 napoleon_use_admonition_for_notes = True
 napoleon_use_admonition_for_references = True
 napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-napoleon_preprocess_types = False
-napoleon_type_aliases = None
-napoleon_attr_annotations = True
+napoleon_use_param = False
+napoleon_use_rtype = False
+napoleon_use_keyword = False
+napoleon_custom_sections = None
 
 # Autodoc
 autodoc_default_options = {
