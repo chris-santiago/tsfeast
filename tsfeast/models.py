9eb32d2da514478a48a7537561c04bf8b25ee977
chris-santiago <cjsantiago@gatech.edu>
2021-08-05 23:12:01 -0400

    add poisson regressor

--- tsfeast/models.py
+++ tsfeast/models.py
@@ -1,10 +1,11 @@
 """Module for Scikit-Learn Regressor with ARMA Residuals."""
-from typing import Tuple
+from typing import Tuple, Optional
 
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, PoissonRegressor
 from sklearn.linear_model._base import LinearModel
 from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.ar_model import AutoReg, AutoRegResults
 
 from tsfeast._base import SciKitContainer, StatsModelsContainer
 from tsfeast.utils import Data
@@ -61,3 +62,42 @@ class TSARegressor(StatsModelsContainer):
     def _predict(self, X: Data) -> Data:
         """Predict the response."""
         return self.fitted_model_.forecast(steps=X.shape[0])
+
+
+class PoissonAutoReg(SciKitContainer):
+    """Estimator for Generalized Poisson model with autoregressive residuals."""
+    def __init__(self, lags: int = 1):
+        """Constructor"""
+        super().__init__()
+        self.lags = lags
+        self._poisson_fit: Optional[PoissonRegressor] = None
+        self._autoreg_fit: Optional[AutoRegResults] = None
+
+    def _fit_poisson(self, X, y) -> None:
+        """Fit a Poisson model."""
+        mod = PoissonRegressor()
+        self._poisson_fit = mod.fit(X, y)
+        self.intercept_ = mod.intercept_
+        self.coef_ = mod.coef_
+        self.resid_ = np.ravel(y) - mod.predict(X)
+
+    def _fit_autoreg(self) -> None:
+        """Fit an AR model."""
+        mod = AutoReg(self.resid_, self.lags)
+        self._autoreg_fit = mod.fit()
+
+    def _fit(self, X: Data, y: Data) -> "PoissonAutoReg":
+        """Fit the estimator."""
+        self._fit_poisson(X, y)
+        self._fit_autoreg()
+        return self
+
+    def _predict(self, X: Data) -> np.ndarray:
+        """Predict the response."""
+        if self._poisson_fit is None:
+            raise AttributeError('Model must be fit before predicting.')
+        if self._autoreg_fit is None:
+            raise AttributeError('Model must be fit before predicting.')
+        poisson_pred = self._poisson_fit.predict(X)
+        autoreg_pred = self._autoreg_fit.forecast(len(X))
+        return poisson_pred + autoreg_pred
